8f3598cce2890c6190f94269a2453e2a4944b499
Damianp99 <damianperez200199@gmail.com>
2022-01-11 17:47:45 +0100

    printed on console word lenght

---
+++ js/snack2.js
@@ -0,0 +1,14 @@
+// L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+const firstWord = prompt('Inserisci una parola').trim();
+const secondWord = prompt('Inserisci una parola').trim();
+
+
+
+if (firstWord.length > secondWord.length) {
+    console.log(secondWord, firstWord)
+} else if (firstWord.length < secondWord.length) {
+    console.log(firstWord, secondWord)
+
+}
